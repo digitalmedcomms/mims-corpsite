8c847803295528ceeab433021222b053d6757dd8
Paul John Angat <pauljohn.angat@mims.com>
2026-07-30 12:00:34 +0800

    message from ceo page

--- app/Views/Pages/message-from-ceo.php
+++ app/Views/Pages/message-from-ceo.php
@@ -1,12 +1,12 @@
 <div id="ceo-message-container">
     <div class="banner" style="">
         <div class="container">
-            <h1 class="text-blue">Message from CEO</h1>
+            <h1 class="text-blue">Message from the CEO</h1>
             <div class="breadcrumbs">
                 <ul>
                     <li><a href="<?php echo base_url(); ?>" class="breadcrumb-link">Home</a></li>
                     <li><a href="<?php echo base_url('about-us'); ?>" class="breadcrumb-link">About Us</a></li>
-                    <li><a href="javascript:;" class="breadcrumb-link">Message from CEO</a></li>
+                    <li><a href="javascript:;" class="breadcrumb-link">Message from the CEO</a></li>
                 </ul>
             </div>
         </div>
