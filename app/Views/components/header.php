d4b639033016956de7bc42429c6cec0e31917e23
Paul John Angat <pauljohn.angat@mims.com>
2026-08-28 11:04:27 +0800

    update

--- app/Views/components/header.php
+++ app/Views/components/header.php
@@ -81,8 +81,25 @@
         gtag('js', new Date());
 
         gtag('config', 'G-PEFBHN6P08');
-        </script>
+
+        _linkedin_partner_id = "10818433";
+        window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+        window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+
+        (function(l) {
+            if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                window.lintrk.q=[]}
+                var s = document.getElementsByTagName("script")[0];
+                var b = document.createElement("script");
+                b.type = "text/javascript";b.async = true;
+                b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                s.parentNode.insertBefore(b, s);
+        })(window.lintrk);
+    </script>
 </head>
 <body data-scroll-animation="true">
+<noscript>
+<img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=10818433&fmt=gif" />
+</noscript>
 <div class="tp-home">
     <?php echo view('components/navigation_bar', ['nav' => (isset($nav) ? $nav : '')]); ?>
